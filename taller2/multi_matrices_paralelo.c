8793c7cb61eb436a58d2e34aa672d15caded7e01
anaguicas <angelicaaguicas@gmail.com>
2016-03-04 11:52:17 -0500

    Update multi_matrices_paralelo.c

--- taller2/multi_matrices_paralelo.c
+++ taller2/multi_matrices_paralelo.c
@@ -12,14 +12,15 @@
 
 __global__ void multiplicar(int *m1_a, int *m2_a, int *m3_a){
 	
-	int i,j,aux;
+  int i,j,aux;
 
-	j=(threadIdx.x*blockDim.x) + threadIdx.x;
-	i=(threadIdx.y*blockDim.y) + threadIdx.y;
+  j = blockIdx.y*blockDim.y+threadIdx.y;
+  i = blockIdx.x*blockDim.x+threadIdx.x;
 	
 	if(i<cols && j<rows){
-		for(int k=0;k<cols;k++){
-		 aux+= m1_a[i*cols+j] * m2_a[j*cols+j];
+    	aux=0;
+	   for(int k=0;k<cols;k++){
+		 aux+= m1_a[j*cols+k] * m2_a[k*cols+i];
 		}
 		m3_a[i*cols+j]=aux;
 	}
@@ -30,7 +31,7 @@ void llenar(int *m){
 	int i,j;
 	for (i = 0; i < rows; i++){
 	   for (j = 0; j < cols; j++){
-		m[i*cols+j]=i+j;		
+			m[(i*cols)+j]=i+j;		
 		}
 	}
 }
@@ -39,7 +40,7 @@ void imprimir(int *m){
 	int i,j;
 	for (i = 0; i < rows; i++){		
 		for (j = 0; j < cols; j++){
-			printf("%d ",m[i*cols+j]);
+			printf("%d ",m[(i*cols)+j]);
 		}
 		printf("\n");		
 	}
@@ -60,7 +61,9 @@ int main(){
 
 	llenar(m1);
 	llenar(m2);
-	
+  
+	start_t=clock();
+  
 	cudaMalloc((void **)&m1_a,(tama));
 	cudaMalloc((void **)&m2_a,(tama));
 	cudaMalloc((void **)&m3_a,(tama));
@@ -70,9 +73,7 @@ int main(){
 
 	int blockSize=32;
 	dim3 dimBlock(blockSize,blockSize,1);
-    	dim3 dimGrid(ceil(cols/float(blockSize)),ceil(cols/float(blockSize)),1);
-	
-	start_t=clock();
+  	dim3 dimGrid(ceil(cols/float(blockSize)),ceil(cols/float(blockSize)),1);
 
 	multiplicar<<<dimGrid,dimBlock>>>(m1_a,m2_a,m3_a);
 
@@ -81,13 +82,14 @@ int main(){
 	//multiplicar(m1_a,m2_a,m3_a);
 
 	end_t= clock();
+  	//imprimir(m1);
+  	//printf("\n");
 	//imprimir(m2);
+  	//printf("\n");
 	imprimir(m3);
 	printf("\n");
 	total_t= ((double)(end_t - start_t))/CLOCKS_PER_SEC;
 	printf("tiempo %f",total_t);
-	
-	
 	
 	cudaFree(m1_a);
 	cudaFree(m2_a);
